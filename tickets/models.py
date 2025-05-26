7dd95fc19499223b7b2c7f039f3c4c36505e1584
Denver Brierley <32299901+DenverBrierley@users.noreply.github.com>
2025-05-26 02:39:03 +0100

    created ticket model

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Tickets(models.Model):
+    ticket_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    severity = models.IntegerField()
+    status = models.CharField(max_length=50)
